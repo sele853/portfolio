720e86800ba1226f5d9725310192d1fc4510773e
saladin <solehadinmoh@gmail.com>
2025-03-08 18:13:24 +0300

    Implemented homepage structure and added text animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
+import { TypeAnimation } from "react-type-animation";
+
 function Home() {
-    return (
-        <div>
-            home page
+  return (
+    <div className="flex flex-col items-center h-screen justify-center gap-y-5">
+      <h1 className="text-6xl font-serif font-Mulish">Solehadin Mohammed</h1>
+      <div>
+        <span className="text-3xl font-[Mulish]">i am  </span>
+        <TypeAnimation
+        sequence={[
+            'full stack developer', 
+            3000, 
+            'web developer', 
+            3000, 
+            'competitive programmer ', 
+            3000, 
+            
+            ]}
+            wrapper="span"
+            speed={50}
+            className="text-3xl font-bold"
+            repeat={Infinity}
+            />
         </div>
-    )
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -9,44 +9,44 @@ import { FaTelegramPlane } from "react-icons/fa";
 
 function Sidebar() {
     return (
-      <div className="bg-gray-900 w-16 text-white flex flex-col items-center">
+      <div className="bg-gray-900 w-12 text-white flex flex-col items-center">
          <ul className=" flex flex-col gap-y-6 items-center">
            <li>
-              <img src="../data/logo-1.jpg" className="h-16 w-auto" />
+              <img src="./logo.jpg" className="h-12 w-auto" />
            </li>
            <li>
               <Link to='/'>
-                 <AiOutlineHome className="text-2xl"/>
+                 <AiOutlineHome className="text-xl"/>
               </Link>
            </li> 
            <li>
               <Link to='/about' >
-                 <RxPerson className="text-2xl"/>
+                 <RxPerson className="text-xl"/>
               </Link>
            </li> 
            <li>
               <Link to='/resume' >
-                 <IoSettingsOutline className="text-2xl"/>
+                 <IoSettingsOutline className="text-xl"/>
               </Link>
            </li>
            <li>
               <Link to='/portfolio' >
-                 <LiaEye className="text-2xl"/>
+                 <LiaEye className="text-xl"/>
               </Link>
            </li>
            <li>
               <Link to='/contact' >
-                 <VscMail  className="text-2xl"/>
+                 <VscMail  className="text-xl"/>
               </Link>
            </li> 
         </ul>
         <div className="flex-1"></div>
         <div className="flex flex-col gap-y-4 pb-6">
            <a href="https://github.com/sele853" target="_blank" rel="noopener noreferrer" >
-              <FaGithub  className="text-xl"/>
+              <FaGithub  className="text-lg"/>
            </a>
            <a href="https://t.me/sele7853" target="_blank" rel="noopener noreferrer">
-              <FaTelegramPlane className="text-xl"/>
+              <FaTelegramPlane className="text-lg"/>
            </a>
         </div>
       </div>
